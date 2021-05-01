543bd11830d27fcad64a03471f39ae76ddac286c
Abdusselam Albayrak <74913844+AlbayrakA96@users.noreply.github.com>
2021-05-02 00:42:59 +0200

    added Header component added Home component added video images files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu></NavMenu>
+    </Nav>
+  );
 };
 
 export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  alignitems: center;
+  padding 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div``;
